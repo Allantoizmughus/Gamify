0db73ffd04a8dd3b4079f972fd6c3673c8d5ed55
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-04 02:31:39 +0300

    Use ButterKnife to bind all the views of mainActivity

--- app/src/main/java/com/moringaschool/gamify/MainActivity.java
+++ app/src/main/java/com/moringaschool/gamify/MainActivity.java
@@ -2,13 +2,24 @@ package com.moringaschool.gamify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.util.ULocale;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.ViewCategoryButton) Button mViewCategoryButton;
+    @BindView(R.id.category) EditText mCategory;
+    @BindView(R.id.AppName) TextView mAppName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
