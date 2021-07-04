f9c52dfe8695e71ecce7590997b39ee209b5fffd
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-04 03:05:08 +0300

    Create a new GameListActivity

--- app/src/main/java/com/moringaschool/gamify/MainActivity.java
+++ app/src/main/java/com/moringaschool/gamify/MainActivity.java
@@ -2,8 +2,10 @@ package com.moringaschool.gamify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.icu.util.ULocale;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,15 +13,24 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.ViewCategoryButton) Button mViewCategoryButton;
     @BindView(R.id.category) EditText mCategory;
     @BindView(R.id.AppName) TextView mAppName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mViewCategoryButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String location = mCategory.getText().toString();
+        Intent intent = new Intent(MainActivity.this, GamesList.class);
     }
 }
