4afb8b8e6192f4b43569077adbb36e09c6ac6c47
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-04 03:31:56 +0300

    Create an array list of games

--- app/src/main/java/com/moringaschool/gamify/GamesList.java
+++ app/src/main/java/com/moringaschool/gamify/GamesList.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GamesList extends AppCompatActivity {
+    private String[] games = new String[]{"White Tiles","Candy Crush","Pokemon","Word Cookie","Zombie Highway","Car Race","Ludo","Pool Billiard","Fifa 21","T.K.O","Temple Run2","Maze Runner","Escape Room","Fortnite","Arrows"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
