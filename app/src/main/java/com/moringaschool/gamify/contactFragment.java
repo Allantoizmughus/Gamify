6ffe45fd04e4634763283f82bb14f3d0588700b0
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-04 18:43:15 +0300

    Add TextView in fragment xml

--- app/src/main/java/com/moringaschool/gamify/contactFragment.java
+++ app/src/main/java/com/moringaschool/gamify/contactFragment.java
@@ -1,4 +1,17 @@
 package com.moringaschool.gamify;
 
-public class contactFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+public class contactFragment extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_contact, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootView;
+    }
 }
